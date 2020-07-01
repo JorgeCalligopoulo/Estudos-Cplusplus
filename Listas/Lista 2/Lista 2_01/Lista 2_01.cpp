ef2973ac4cdec5b49726fdb6645eed25e0b73d18
Jorge Calligopoulo <calligopoulo@Gmail.com>
2020-07-01 18:58:41 -0300

    Comentarios

--- Listas/Lista 2/Lista 2_01/Lista 2_01.cpp
+++ Listas/Lista 2/Lista 2_01/Lista 2_01.cpp
@@ -1,15 +1,15 @@
 // Lista 2_01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+// faça um programa que leia um numero, incremente 1 a esse numero e o imprima
 #include <iostream>
-using namespace std;
+using namespace std; //delicia
 
-short num;
+short num; // define a ariavel como short,
 
 int main()
 {
-    cout << "Digite um numero!\n";
-    cin >> num;
-    num++;
-    cout << num;
+    cout << "Digite um numero!\n"; // printa uma mensagem
+    cin >> num; // coleta um valor digitado e grava na variavel
+    num++; // acresenta 1 ao numero
+    cout << num; // printa o valor armazenado na variavel
 }
-//end
+//the end
--- Listas/Lista 2/Lista 2_02/Lista 2_02.cpp
+++ Listas/Lista 2/Lista 2_02/Lista 2_02.cpp
@@ -1,27 +1,27 @@
 // Lista 2_02.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// utilizando a biblioteca cmath faça um programa que leia um numero inteiro e imprima: oquadrado do numero ; A raiz quadrada; o seno; o cosseno
 
 #include <iostream>
 #include <cmath>
-using namespace std;
+using namespace std;// gostosa
 
-int num;
+int num; // variavel do tipo inteira para numeros inteiros
 
 int main()
 {
-	cout << "Digite um numero inteiro e positivo!\n";
-ponto01:
-	cin >> num;
-	if (num < 0)
+	cout << "Digite um numero inteiro e positivo!\n"; // texto
+ponto01: // marcador
+	cin >> num; // grava um numero na memoria
+	if (num < 0) // verifica 
 	{
-		cout << "numero invalido!!\n Digite um numero inteiro e positivo!!\n";
-		goto ponto01;
+		cout << "numero invalido!!\n Digite um numero inteiro e positivo!!\n"; // mensagem ao usuario
+		goto ponto01; //retrocede o codigo ate o marcador
 	}
-	cout << "numero Digitado: " << num << endl;
-	cout << num << " ao quadrado:\t " << pow(num, 2) << endl;
-	cout << "Raiz de " << num << ":\t " << sqrt(num) << endl;
-	cout << "Seno de " << num << ":\t " << sin(num) << endl;
-	cout << "Cosseno de " << num << ":\t " << cos(num) << endl;
+	cout << "numero Digitado: " << num << endl; // imprime o numero armazenado no vetor
+	cout << num << " ao quadrado:\t " << pow(num, 2) << endl;  // imprime o numero ao quadrado
+	cout << "Raiz de " << num << ":\t " << sqrt(num) << endl;  // imprime a raiz do numero
+	cout << "Seno de " << num << ":\t " << sin(num) << endl;   // imprime o seno
+	cout << "Cosseno de " << num << ":\t " << cos(num) << endl;// imprime o cosseno
 
 }
 

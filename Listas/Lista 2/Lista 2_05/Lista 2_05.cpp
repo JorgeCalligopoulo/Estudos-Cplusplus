06d5041b78a596494d2b76ddd7b611f82169b5bb
Jorge Calligopoulo <calligopoulo@Gmail.com>
2020-07-03 17:00:30 -0300

    5 feito

--- Listas/Lista 2/Lista 2_05/Lista 2_05.cpp
+++ Listas/Lista 2/Lista 2_05/Lista 2_05.cpp
@@ -3,18 +3,27 @@
 
 #include <iostream>
 
+using namespace std;//...
+
+int num, num1;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Digite dois numeros inteiros separados por enter!\n"; // msg ao usuario
+    cin >> num >> num1; // captura duas entradas do teclado e atribui as respectivas variaveis
+	if (num > num1) // condisional que verifica se o 1 numero é maior que o segundo
+	{
+		cout << "o maior e: " << num << " e a diferença e: " << (num - num1) << endl; // emprime o maior assim como a diferença
+	}
+	else if (num1 > num) // condisional que verifica se o 2 numero é maior que o 1
+	{
+		cout << "o maior e: " << num1 << " e a diferença e: " << (num1 - num) << endl; // emprime o maior assim como a diferença
+	}
+	else // caso não entre nas condicionais anteriores é capturado por esta
+	{
+		cout << "os valores digitados sao iguai!!!\n assim sendo a diferença e nula\n"; // caso os dois imputs sejam iguais exibe esta msg
+	}
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

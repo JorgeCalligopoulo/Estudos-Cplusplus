f6ef81932cff2140cbcec9c4881d09115196613d
Jorge Calligopoulo <calligopoulo@Gmail.com>
2020-07-03 16:46:50 -0300

    4 finalizado

--- Listas/Lista 2/Lista 2_04/Lista 2_04.cpp
+++ Listas/Lista 2/Lista 2_04/Lista 2_04.cpp
@@ -1,20 +1,24 @@
 // Lista 2_04.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <math.h>
+
+float num;
+
+using namespace std;//...
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Digite um numero!\n"; // mostra uma mensagem ao usuario
+	cin >> num; //grava um numero digitado na memoria
+	if (num > 0) //inicia uma condicional 
+	{
+		cout << "A raiz quadrada de " << num << " e:  " << sqrt(num) << endl; // emprime a raiz do numero
+
+	}
+	else // caso não seja aceito pela condição anterior, é tratado por esta
+	{
+		cout << num << "ao quadrado e:  " << pow(num, 2) << endl; // emprime o numero ao quadrado
+	}
+	return 0; // the end
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

0817f68fe00184a986d40118b4c31f40e913f308
Jorge Calligopoulo <calligopoulo@Gmail.com>
2020-07-18 08:32:55 -0300

    lista 3 3 feito

--- Listas/Lista 3/Lista 3_03/Lista 3_03.cpp
+++ Listas/Lista 3/Lista 3_03/Lista 3_03.cpp
@@ -7,8 +7,26 @@ imprima sua média.
 #include <iostream>
 using namespace std;// subarashi
 
+void mediadez();
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "digite os numeros a serem tirados a media, valores negativos serão ignorados!\n";
+	mediadez();
+	return 0;
+}
+
+void mediadez()
+{
+	int num, cont, soma; num = cont = soma = 0;
+
+	while (cont < 10)
+	{
+		cout << "numero: ";
+		cin >> num;
+		cont += (num >= 0);
+		soma += ((num >= 0)*num);
+	}
+	cout << "A media dos 10 numeros positivos e: " << soma / 10 << endl;
 }
 

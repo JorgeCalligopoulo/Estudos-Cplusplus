b3a5e86598175683d110eff3cfe812d97199b090
Jorge Calligopoulo <calligopoulo@Gmail.com>
2020-07-25 22:13:04 -0300

    Refined, stiil need to debug "Fatorial"

--- Triangulo de pascal/Pascal/Pascal.cpp
+++ Triangulo de pascal/Pascal/Pascal.cpp
@@ -6,16 +6,25 @@
 int fatorial(int x);
 int pascal(int n, int p);
 
-int size;
+int size,space;
 
 int main()
 {
     
-    std::cout <<"Digite o tamanho da piramide (Limite 12)\nTamanho: " << std::endl;
+    std::cout <<"Digite o tamanho da piramide (Limite 12)\nTamanho: ";
     std::cin >> size;
+    space = size;
 
     for (int n = 0; n < size; n++)
     {
+
+        for (int i = 0; i < (space); i++)
+        {
+            std::cout << " ";
+        }
+
+        space--;
+        
         for (int p = 0; p < (n+1); p++)
         {
             std::cout << pascal(n,p)<< " ";
